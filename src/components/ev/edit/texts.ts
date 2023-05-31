9a3a1b26ee5b09ef879f01c2fa32b10f1599e174
Jon-Inge Heggstad <jon.heggstad@gmail.com>
2023-05-31 13:20:18 +0200

    New features: add and edit vehicles

---
+++ src/components/ev/edit/texts.ts
@@ -0,0 +1,32 @@
+export const texts = {
+    title: {
+        nb: 'Rediger bilinformasjon',
+        en: 'Edit car information',
+    },
+    info: {
+        nb: 'Bildata brukes for å regne ut ladeplan',
+        en: 'Car data is used to calculate charging plan',
+    },
+    button_delete: {
+        nb: 'Slett bil',
+        en: 'Delete car',
+    },
+    button_cancel: {
+        nb: 'Avbryt',
+        en: 'Cancel',
+    },
+    button_save: {
+        nb: 'Lagre',
+        en: 'Save',
+    },
+    modal: {
+        header: {
+            nb: 'Du er i ferd med å slette bilen',
+            en: "You're about to delete the car",
+        },
+        content: {
+            nb: 'Ladeplaner vil fra nå av bli utregnet basert på generisk bildata.',
+            en: 'Charging plans will from now on be calculated based on generic car data.',
+        },
+    },
+}
